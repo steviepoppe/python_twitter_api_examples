5eede304f4b5eceae72a40705921ff860611dc25
Stevie Poppe <stevie_poppe@hotmail.com>
2020-05-18 23:14:43 +0200

    Update python_parse_tweet.py

--- python_parse_tweet.py
+++ python_parse_tweet.py
@@ -13,32 +13,60 @@ def parse_tweets(file_name):
 		with open('./results/%s.csv' % file_name, mode='w', encoding="utf-8",newline='') as file:
 
 			writer = csv.writer(file)
-			writer.writerow(["text", "hashtags", "created_at", "is_retweet", "user_screen_name", "user_description", "user_friends_count", "user_followers_count", "user_total_tweets", "user_created_at"])
+
+			writer.writerow(["tweet_id","text", "hashtags", "created_at", "is_retweet", "user_screen_name", 
+				"user_description", "user_friends_count", "user_followers_count", 
+				"user_total_tweets", "user_created_at"])
 
 			tweets = json.load(tweet_data)
 
 			for tweet in tweets["objects"]:
-				user_screen_name = tweet["user"]["screen_name"]
-				user_description = tweet["user"]["description"]
-				user_friends_count = tweet["user"]["friends_count"]
-				user_followers_count = tweet["user"]["followers_count"]
-				user_total_tweets = tweet["user"]["statuses_count"]
-				user_created_at = tweet["user"]["created_at"]
+				
+				tweet_id = tweet["id"]
+				entities = tweet["entities"]
+
+				user = tweet["user"]
+				user_screen_name = user["screen_name"]
+				user_description = user["description"].strip()
+				user_following_count = user["friends_count"]
+				user_followers_count = user["followers_count"]
+				user_total_tweets = user["statuses_count"]
+				user_created_at = string_to_dt(user["created_at"])
 				created_at = string_to_dt(tweet["created_at"])
 				retweet_count = tweet["retweet_count"]
-				hashtags = []
-				if len(tweet["entities"]["hashtags"]) > 0:
-					for hashtag in tweet["entities"]["hashtags"]:
-						hashtags.append(hashtag["text"])
-				#converts hashtag dict to comma-seperated string, can be commented out if original list is preferred
-				hashtags = ','.join(hashtags)
-				text = (tweet["extended_tweet"]["full_text"] if "extended_tweet" in tweet else tweet["full_text"] if "full_text" in tweet else tweet["text"])
+
 				is_retweet = ("retweeted_status" in tweet)				
-				is_quote = ("quoted_status" in tweet)
+				#is_quote = ("quoted_status" in tweet)
 
-				quoted_text = ("und" if not is_quote else tweet["quoted_status"]["full_text"] if "full_text" in tweet["quoted_status"] else tweet["quoted_status"]["text"])
+				hashtags = ()
 
-				writer.writerow([text, hashtags, created_at, is_retweet, user_screen_name, user_description, user_friends_count, user_followers_count, user_total_tweets, user_created_at])
+				if is_retweet == True:
+					retweet = tweet["retweeted_status"]
+					retweet_original_id = retweet["id"]
+					re_entities = retweet["entities"]
+
+					text = "RT @" + entities["user_mentions"][0]["screen_name"] + ": " + (retweet["extended_tweet"]["full_text"] 
+						if "extended_tweet" in retweet else retweet["full_text"] if "full_text" in retweet else retweet["text"])
+					
+					if 'hashtags' in re_entities:
+						hashtags = (hashtag["text"] for hashtag in re_entities["hashtags"])
+				else:					
+					text = (tweet["extended_tweet"]["full_text"] if "extended_tweet" in tweet 
+					else tweet["full_text"] if "full_text" in tweet else tweet["text"])		
+
+					if 'hashtags' in entities:
+						hashtags = (hashtag["text"] for hashtag in entities["hashtags"])
+
+				#converts hashtag dict to comma-seperated string, can be commented out if original list is preferred
+				hashtags = ', '.join(hashtags)
+
+				text = text.strip()
+
+				#quoted_text = ("und" if not is_quote else tweet["quoted_status"]["full_text"] 
+				#	if "full_text" in tweet["quoted_status"] else tweet["quoted_status"]["text"])
+
+				writer.writerow([tweet_id, text, hashtags, created_at, is_retweet, user_screen_name, user_description, 
+					user_following_count, user_followers_count, user_total_tweets, user_created_at])
 
 	print("Finished. Saved to ./results/%s_tweets.csv" % (file_name))
 
