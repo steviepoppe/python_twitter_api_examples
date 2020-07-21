0321737c9094868766a2f3c2a6b33dd65609f7be
Stevie Poppe <stevie_poppe@hotmail.com>
2020-07-21 03:29:46 +0200

    Update python_twitter_search.py

--- python_twitter_search.py
+++ python_twitter_search.py
@@ -1,42 +1,111 @@
 import tweepy
 import sys
 import json
+import os
 from datetime import datetime
 from pathlib import Path
+
+# This python script accesses the Twitter Search API using the third-party python plug-in tweepy
+# When running from the command prompt, it expects at least one argument: the search query
+# (optionally, just input the search query in the global variable search_query)
+# Results are saved as raw JSON objects in a './results/' subdirectory (contains a ton of valuable meta-data)
+# In order to keep the filesize of the results manageable (and bypass a tweepy memory leak), 
+# objects are saved per 10k (re)tweets at a time
+# Names of the saved files are based on the search query + part number + current datetime 
+# The process can be terminated at any time using ctrl-c; the last ID will be printed before exit
+
+# Use the last ID as max_id to continue at a latter time
+# When continuing the process at a latter time (within a timespan of a week), use the first tweet_id
+# of the first tweet in the first JSON file as the since_id value
 
 #Twitter API credentials
 consumer_key = 'your_consumer_key_here'
 consumer_secret = 'your_consumer_secret_here'
 
-def search_tweets(search_query):
+max_counter = 10001 
+max_id = None #Optional: Until which ID?
+since_id = None #Optional: Since which ID?
+language = 'ja' #Optional: filtering by which language? Japanese? -> 'ja'
+search_query = 'China'
+quit = False
+
+def search_tweets(sys_args):
+
+	global max_id
+	global since_id
+	global search_query
+	global language
+
+	if len(sys_args) > 1:
+		search_query = sys_args[1]
+	if len(sys_args) > 2:
+		max_id = sys_args[2]
+	if len(sys_args) > 3:
+		since_id = sys_args[3]
+	if len(sys_args) > 4:
+		language = sys_args[4]
 
 	auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
 	api = tweepy.API(auth, wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
 
-	tweet_count = 0
+	tweet_total_count = 0
 	timestamp = datetime.today().strftime('%Y%m%d_%H%M%S')
-	
-        Path("./results/").mkdir(parents=True, exist_ok=True)
-	
-	with open('./results/search_tweets_%s_%s.json' % (search_query, timestamp), mode='w', encoding="utf-8") as file:
-		#bit of a  hacky way to create valid JSON but easier on memory
+	part = 0
+	tweet_count = None;
+
+	#create dir results if != exists
+	Path("./results/").mkdir(parents=True, exist_ok=True)
+
+	while tweet_count != 0 and quit == False:
+		part += 1
+		tweet_count = process_tweets(api, search_query, timestamp, part)
+		tweet_total_count += tweet_count
+
+	#To do: save last tweet ID in the registry in order to automatize with batch scripts
+	print ("Finished process. Downloaded %d total tweets. Last tweet ID was %s" % (tweet_total_count, max_id))
+
+def process_tweets(api, search_query, timestamp, part):
+	global max_id 
+	global quit 
+	with open('./results/search_tweets_%s_%s_part-%s.json' % (search_query, timestamp, part), 
+		mode='w', encoding="utf-8") as file:
+		#bit of a hacky way to create valid JSON but easier on memory
 		file.write('{"objects":[') 
+		tweet_count = 0
 		try:
-			# cursor pagination, 100 is limit of returned Tweets per access call
-			for status in tweepy.Cursor(api.search,q=search_query, count=100, tweet_mode='extended').items():
-				#set ensure_ascii to true to encode Unicode in ascii. ',' conditional operator is part of the manual JSON parsing hack
-				file.write((',' if tweet_count > 0 else '') +json.dumps(status._json,ensure_ascii=False,sort_keys = True,indent = 4))
-				tweet_count += 1
-				if tweet_count % 100 == 0:
-					print("Downloaded %d tweets" % tweet_count)	
+			# cursor pagination, 100 is limit of returned tweets per access call
+			for status in tweepy.Cursor(api.search,q=search_query, count=100, 
+				tweet_mode='extended',
+				lang=language,
+				since_id=since_id,
+				max_id=max_id
+				).items(max_counter):
+				# tweepy takes max_id as first id to return: already have this so skip 
+				# (that's also why the max_counter is 10001 instead of 10k)
+				if max_id != status.id_str:
+					max_id = status.id_str
+					#conditional operator is part of the manual JSON parsing hack
+					file.write((',' if tweet_count > 0 else '') + json.dumps(status._json,
+						ensure_ascii=False,sort_keys = True,indent = 4))
+					tweet_count += 1
+					if tweet_count % 100 == 0:
+						print("Downloaded %d tweets" % tweet_count)	
 		except KeyboardInterrupt:
-			print("Process terminated.")
-						
+			print("Process terminated. Last tweet ID was %s" % max_id)
+			quit = True
+		except tweepy.TweepError:
+			print("Memory error. Last tweet ID was %s" % max_id)
 		file.write(']}')
 
-	print ("Downloaded %d tweets, Saved to ./results/search_tweets_%s_%s.json" % (tweet_count, search_query, timestamp))
+	#To do: we don't know if we've reached the last tweet until the tweepy API call, which happens after creating
+	# a new JSON file. For now, this just removes the empty, final json file
+	#optionally we could keep all objects, per chunk of 10k, in memory and save at the end but this is way more memory-taxing
+	if tweet_count > 0:
+		print ("Downloaded %d tweets, Saved to ./results/search_tweets_%s_%s_part-%s.json" % (tweet_count, search_query, timestamp, part))
+	else:
+		os.remove("./results/search_tweets_%s_%s_part-%s.json" % (search_query, timestamp, part))
+
+	return tweet_count
 
 if __name__ == '__main__':
-	#pass in the keyword to search tweets	
-	search_tweets(sys.argv[1])
-	
+	search_tweets(sys.argv)
